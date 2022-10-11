acbc98a66df13fe2f7731a1c87df5fc5f1069f4e
rnagao <nagao1234@poli.ufrj.br>
2022-10-11 09:06:53 +0100

    Simplify Test Setup

--- src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -1,16 +1,22 @@
 package com.aor.numbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ListDeduplicatorTest {
+    List<Integer> list = new ArrayList<>();
 
+    @BeforeEach
+    public void setupList() {
+        list = Arrays.asList(1,2,4,2,5);
+    }
     @Test
     public void deduplicate() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
         List<Integer> expected = Arrays.asList(1,2,4,5);
 
         ListDeduplicator deduplicator = new ListDeduplicator();
